cc44e6aa7ce7f97b8bacef14c2265ee983eb57db
Mina <minashahzoda@gmail.com>
2026-01-27 17:10:44 +0900

    feat: organize and test pages router system

---
+++ pages/community/index.tsx
@@ -0,0 +1,13 @@
+import { useState} from "react";
+
+
+const Community = () => {
+    console.log("COMMUNITY COMPONENT-pages router");
+    const [title, setTitle] = useState<string>("hello");
+    
+    return <div>COMMUNITY
+        <button onClick={() => alert("Hello MIT")} style={{ margin: "15px" }}>PressMe</button>
+    </div>
+}
+
+export default Community;
