76c827de974792a61a45cc070767d59e18628a4e
Mina <minashahzoda@gmail.com>
2026-01-30 19:00:24 +0900

    feat: sass integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import { ThemeProvider } from "@mui/material/styles";
 import { createTheme, CssBaseline } from "@mui/material";
 import { light } from "../scss/MaterialTheme";
 import { useState } from "react";
+import "../scss/app.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
   //@ts-ignore
